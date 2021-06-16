3835400a9b2bc67e2106ab769b9fce7b94bb27d4
Monika Sharma <monika2004sharma@gmail.com>
2021-06-16 05:45:02 +0530

    update

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const http = require('http');
 const app = require('./app');
 const server = http.createServer(app);
-const PORT = 8000;
+const PORT = 3000;
 
 server.listen(PORT, () => console.log(`server started on port : ${PORT}`));
