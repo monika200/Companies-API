a10f4fc126ede4cd5a9dd91eb8133a97088f7603
MONIKA SHARMA <monika2004sharma@gmail.com>
2021-06-17 00:02:07 +0530

    Update companies.js

--- routes/companies.js
+++ routes/companies.js
@@ -67,7 +67,7 @@ router.get('/companies_details/:name', async (req, res) => {
 	try {
 		const client = await mongoClient.connect(DB_URL);
 		const db = client.db('companies');
-		const result = await db.collection('company').findOne({"name": {$regex:(req.params.name)}});
+		const result = await db.collection('company').findOne({"name": {$regex:(req.params.name)}},{"_id":0,"description":0,"url_image":0,"rating":0});
 		if (!result) {
 			res.json({ message: 'No Matching Company Found' });
 		} else {
